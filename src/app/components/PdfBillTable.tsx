8520173b181bf53b234da9298e81e64dfbdfa5dd
yuvra <yuvraj.mane@yahoo.com>
2023-11-14 17:11:38 +0530

    Word Length increase

--- src/app/components/PdfBillTable.tsx
+++ src/app/components/PdfBillTable.tsx
@@ -57,8 +57,8 @@ const ReportTable = ({ data }: any) => {
 			</View>
 			{data.map((row: any, i: any) => {
 
-				const itemDescription = row['Item Description'];
-				const itemDescriptionArr = itemDescription.match(/.{1,9}/g)
+				const itemDescription: any ='aaaaaaaaaaaaaaaajknjknjjjjjjjjjjjjajajajajajajajajjaa';
+				const itemDescriptionArr = itemDescription.match(/.{1,40}/g)
 				finalTotalAmount = finalTotalAmount + row.Total;
 
 				return (
